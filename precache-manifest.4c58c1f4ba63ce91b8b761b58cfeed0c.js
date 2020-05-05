47cb4599fc30e52fe42380375edb17233bf5e1af
Binh Nguyen <binhnd@Binhs-MacBook-Pro.local>
2020-05-05 23:52:47 +0700

    Updates

---
+++ precache-manifest.4c58c1f4ba63ce91b8b761b58cfeed0c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d5b0df9181e03d4812f3bc548156a41",
+    "url": "/view-reactjs/index.html"
+  },
+  {
+    "revision": "63c116809975e555b631",
+    "url": "/view-reactjs/static/js/2.646e66fd.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/view-reactjs/static/js/2.646e66fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76c1b09b4b9eadeb9019",
+    "url": "/view-reactjs/static/js/main.edc53e7c.chunk.js"
+  },
+  {
+    "revision": "ff97a2526c9a28faad6f",
+    "url": "/view-reactjs/static/js/runtime-main.61fec981.js"
+  }
+]);
